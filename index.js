e25da9e34fe8e83ae7f17be24a8c435aa138065f
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-03-29 20:52:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
+const random = Math.floor(Math.random() * 10) +1;
+
+let num3 = 24;
+let num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(7,20,13,19,3)
 // Write your code here
